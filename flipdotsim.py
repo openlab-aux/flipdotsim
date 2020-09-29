f6776b42d6525f0f2a678b8d46c35e84987f65d8
sternenseemann <0rpkxez4ksa01gb3typccl0i@systemli.org>
2020-09-29 12:49:21 +0200

    refactor(flipdotsim): remove trailing whitespace

--- flipdotsim.py
+++ flipdotsim.py
@@ -6,9 +6,9 @@ import pygame
 from pygame.locals import *
 
 class FlipdotSim():
-    def __init__(self, 
+    def __init__(self,
                  imageSize = (80,16),
-                 pixelSize = 10, 
+                 pixelSize = 10,
                  udpPort = 2323):
         self.udpPort = udpPort
         self.flipdotMatrixSimulatorWidget = FlipdotMatrixSimulatorWidget(imageSize, pixelSize)
@@ -19,13 +19,13 @@ class FlipdotSim():
         self.RunServer()
 
     def RunServer(self):
-        try: 
+        try:
             while True:
                 rawData = self.udpHostSocket.recv(4096)
                 imageArray = ImageArrayAdapter().convertPacketToImageArray(rawData)
                 self.flipdotMatrixSimulatorWidget.show(imageArray)
-        finally: 
-            self.udpHostSocket.close() 
+        finally:
+            self.udpHostSocket.close()
 
 class ImageArrayAdapter():
     def __init__(self):
@@ -37,13 +37,13 @@ class ImageArrayAdapter():
         for byte in byteArray:
             self.__appendByteToArrayOfBinaryInts(byte)
         return self.arrayOfBinaryInts
-    
+
     def __appendByteToArrayOfBinaryInts(self, byte):
         byteValue = int(byte)
         for i in range(8):
             if byteValue/(2**(7-i)) > 0:
                 self.arrayOfBinaryInts.append(1)
-            else: 
+            else:
                 self.arrayOfBinaryInts.append(0)
             byteValue = byteValue%(2**(7-i))
 
@@ -51,10 +51,10 @@ class ImageArrayAdapter():
 class FlipdotMatrixSimulatorWidget():
     BLACKCOLOR = 0
     WHITECOLOR = 1
-    
+
     def __init__(self,
                  imageSize = (80,16),
-                 pixelSize = 10): 
+                 pixelSize = 10):
         self.imageSize = imageSize
         self.pixelSize = pixelSize
 
@@ -64,14 +64,14 @@ class FlipdotMatrixSimulatorWidget():
         self.clearPixels()
         pygame.display.update()
         thread.start_new_thread(self.watchCloseThread, ())
-        
+
     def watchCloseThread(self):
         while True:
             for event in pygame.event.get():
                 if event.type in (QUIT, QUIT):
                     import os
                     os.kill(os.getpid(), 9)
-            pygame.time.delay(500)  
+            pygame.time.delay(500)
 
     def show(self, imageArray):
         for yValue in range(self.imageSize[1]):
@@ -85,7 +85,7 @@ class FlipdotMatrixSimulatorWidget():
         for xValue in range(self.imageSize[0]):
             for yValue in range(self.imageSize[1]):
                 self.updatePixel(xValue, yValue, self.BLACKCOLOR)
-        
+
     def updatePixel(self, xValue, yValue, color):
         surface = pygame.Surface((self.pixelSize-1, self.pixelSize-1))
         if color == self.BLACKCOLOR:
@@ -94,6 +94,6 @@ class FlipdotMatrixSimulatorWidget():
                 rectcolor = (255,255,255)
         surface.fill(rectcolor)
         self.screen.blit(surface, (xValue*self.pixelSize, yValue*self.pixelSize))
-        
+
 if __name__ == '__main__':
     FlipdotSim(imageSize=(80,16), pixelSize = 10, udpPort=2323).run()
